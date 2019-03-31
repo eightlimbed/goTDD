bfcbe43b4cfe1e8361c88ba5d5b62ca7d4aac6ef
Lee Gaines <lee_gaines@apple.com>
2019-03-30 19:49:44 -0700

    adds SumAllTail() and tests

--- sum.go
+++ sum.go
@@ -7,3 +7,23 @@ func Sum(numbers []int) (sum int) {
 	}
 	return
 }
+
+// SumAll returns the sum of a slice of slices
+func SumAll(data ...[]int) (sums []int) {
+	for _, numbers := range data {
+		sums = append(sums, Sum(numbers))
+	}
+	return
+}
+
+// SumAllTails returns the sum of a slice of slices, without the first slice
+func SumAllTails(data ...[]int) (sums []int) {
+	for _, numbers := range data {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			sums = append(sums, Sum(numbers[1:]))
+		}
+	}
+	return
+}
--- sum_test.go
+++ sum_test.go
@@ -1,6 +1,9 @@
 package arrays
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
@@ -13,4 +16,37 @@ func TestSum(t *testing.T) {
 		}
 	})
 
+}
+
+func TestSumAll(t *testing.T) {
+
+	t.Run("a collection of one slice", func(t *testing.T) {
+		got := SumAll([]int{1, 2, 3}, []int{4})
+		want := []int{6, 4}
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+}
+
+func TestSumAllTails(t *testing.T) {
+
+	checker := func(got, want []int, t *testing.T) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
+	t.Run("a collection of 4 slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2, 3}, []int{4, 5, 6}, []int{7, 8}, []int{9})
+		want := []int{5, 11, 8, 0}
+		checker(got, want, t)
+	})
+
+	t.Run("handle an empty slice", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2, 3}, []int{}, []int{7, 8}, []int{9})
+		want := []int{5, 0, 8, 0}
+		checker(got, want, t)
+	})
 }
